d04478925cce7327fb57ba817ecda36c32924f00
Kyil Bing <kylidboy@outlook.com>
2020-11-19 18:54:24 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
 require("@openzeppelin/hardhat-upgrades");
 
+const fs = require("fs");
+const infuraKey = "e468cafc35eb43f0b6bd2ab4c83fa688";
+const privateKeys = JSON.parse(
+  fs.readFileSync("ropsten.secret.json").toString().trim()
+);
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
@@ -23,11 +29,8 @@ module.exports = {
     hardhat: {},
     ropsten: {
       chainId: 3,
-      url: "https://ropsten.infura.io/v3/e468cafc35eb43f0b6bd2ab4c83fa688",
-      accounts: [
-        "0ac5211d9a97558e0a929d2b33cb33fd8def2970a9315dc093871b405a64207d", // me
-        "fcd09fc317b13cdf6603bb93853db7e53316bc7c9dc6d4f2cfac1fd093df2f6b", // bobo
-      ],
+      url: `https://ropsten.infura.io/v3/${infuraKey}`,
+      accounts: privateKeys,
     },
   },
   solidity: {
